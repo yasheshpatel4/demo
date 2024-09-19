01a18b5b1dc03c37ac0f7333fba15dfe388ec84f
yashesh <yasheshpatel537@gmail.com>
2024-09-19 17:21:02 +0530

    first

---
+++ app/src/main/java/com/example/myhealth/MyHealth.java
@@ -0,0 +1,15 @@
+package com.example.myhealth;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyHealth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
